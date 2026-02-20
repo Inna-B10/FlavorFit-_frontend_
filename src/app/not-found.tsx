fb3253177bcc29e0764bcf3c3f731e99c037fc1b
Inna-B10 <inna.b10.kodehode@gmail.com>
2026-02-20 12:47:39 +0100

    refactor: update styles in NotFoundPage component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,15 +16,15 @@ export default function NotFoundPage() {
         href='/'
         title='Homepage'
         aria-label='Go to homepage'
-        className='absolute top-5 left-8 hidden transition-all duration-300 ease-in-out hover:scale-102 sm:mt-4 sm:block sm:self-start'
+        className='absolute top-0 left-0 translate-full transition-all duration-300 ease-in-out hover:scale-102 sm:top-5 sm:left-8 sm:translate-0'
       >
         <LogoName className='text-green-dark h-auto w-30 sm:w-42 md:w-62' />
       </Link>
-      <div className='flex flex-col items-center justify-center gap-8 text-[#474747] select-none text-shadow-md'>
-        <div className='flex h-full w-full items-center justify-center gap-5'>
-          <span className='font-sonsie text-[10rem]'>4</span>
+      <div className='w-full space-y-8 text-[#474747] select-none text-shadow-md'>
+        <div className='flex h-full w-full items-center justify-center gap-5 text-[clamp(5rem,20vw,10rem)]'>
+          <span className='font-sonsie'>4</span>
           <div
-            className='collageWrap'
+            className='collageWrap w-25 min-w-20 sm:w-37.5 md:w-50'
             onMouseLeave={() => setQuad('idle')}
           >
             {/* hover zones */}
@@ -73,9 +73,9 @@ export default function NotFoundPage() {
               })}
             </div>
           </div>
-          <span className='font-sonsie -ml-3 text-[10rem]'>4</span>
+          <span className='font-sonsie -ml-3'>4</span>
         </div>
-        <h2 className='font-poppins text-2xl italic'>
+        <h2 className='font-poppins text-center text-[clamp(0.8rem,8vw,1.5rem)] italic'>
           You&apos;ve trespassed into the no-produce zone.
         </h2>
       </div>
